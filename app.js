d144e2274afc362e0d78f2c574a64668eb5a4a82
jerm <jeremie.deletraz@gmail.com>
2020-06-18 18:49:27 +0200

    Server works locally

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
 // Bring all the modules
 const express = require('express');
-const cors = require('cors');
 const fs = require('fs')
 
 // Start the express instance
 const app = express();
 
 // Some middlewares
-app.use(cors());
 app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.json());
 
-// Setting up the routes
+// Setting up the route
 app.get('/', function(req,res){
     // To retrieve the path passed in query in the url we can do the following
-    let path = req.query.path
-    // Do the things
-    // ...
-
-    // Return a response
-    res.json({success: true, msg: 'You gave me this file path in query : '+path})
+    let path = "./"+req.query.path
+    console.log("Request received : Get file at "+path)
+    // Checks if the file exists
+    fs.access(path, fs.F_OK, (err) => {
+        if (err) {
+            console.error(err)
+            res.json({success: false, msg: "Path leads nowhere"})
+        }
+        // File exists. Return it
+        res.download(path);
+    })
 })
-
 
 // Launching the server
 const PORT = 5000;
